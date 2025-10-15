7e1f2b9da041677c56952c3dbdb594ba69d957cc
Jan Bartnicki <jan.bartnicki01@gmail.com>
2025-10-15 20:05:51 +0200

    App init and landing page

---
+++ frontend/steganography/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { LandingPageComponent } from "./shared/components/landing-page/landing-page.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [LandingPageComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent { }

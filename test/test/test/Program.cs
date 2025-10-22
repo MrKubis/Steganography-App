5dfccea75e1c22acafc4ab23df4510f4417a3e67
MrKubis <jakub.f102@gmail.com>
2025-10-22 16:44:25 +0200

    Init Encrypting, Decrypting

---
+++ test/test/test/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
+using System.Text;
+using System.Linq;
+using test;
+
+//MESSAGE PART
+
+string message = "Siemanko!!!";
+string input = "test.jpg";
+string output = "result.jpg";
+LSB lsb = new LSB();
+lsb.EncryptImage(input, output,message);
+lsb.DecryptImage(output);

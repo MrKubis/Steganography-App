8e5ea32ee8fba9a860dff091d4e4bb422eadf189
Jan Bartnicki <jan.bartnicki01@gmail.com>
2025-12-01 20:48:09 +0100

    File upload component

--- frontend/steganography/src/app/shared/components/upload-page/decrypt-component/decrypt-component.component.ts
+++ frontend/steganography/src/app/shared/components/upload-page/decrypt-component/decrypt-component.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { UploadStrategyComponent } from '../strategies/upload-strategy.model';
+import { UploaderComponent } from '../uploader/uploader.component';
 
 @Component({
   selector: 'app-decrypt-component',
-  imports: [],
+  imports: [UploaderComponent],
   templateUrl: './decrypt-component.component.html',
   styleUrl: './decrypt-component.component.scss'
 })
 export class DecryptComponentComponent implements UploadStrategyComponent {
-
+  handleFileUpload(event: { file: File; message?: string }): void {
+    return; // TODO
+  }
 }
--- frontend/steganography/src/app/shared/components/upload-page/encrypt-component/encrypt-component.component.ts
+++ frontend/steganography/src/app/shared/components/upload-page/encrypt-component/encrypt-component.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { UploadStrategyComponent } from '../strategies/upload-strategy.model';
+import { UploaderComponent } from "../uploader/uploader.component";
 
 @Component({
   selector: 'app-encrypt-component',
-  imports: [],
+  imports: [UploaderComponent],
   templateUrl: './encrypt-component.component.html',
   styleUrl: './encrypt-component.component.scss'
 })
 export class EncryptComponentComponent implements UploadStrategyComponent {
-
+  handleFileUpload(event: { file: File; message?: string }): void {
+    return; // TODO
+  }
 }

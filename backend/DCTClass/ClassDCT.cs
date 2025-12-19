c67c80af54a687acf84fc5ea8ad0dee130fde9b8
BartekJ0110 <bartek.janoszka@icloud.com>
2025-12-19 22:57:00 +0100

    feat: Integrate DCT algorithm for JPEG image encryption and decryption

--- backend/DCTClass/ClassDCT.cs
+++ backend/DCTClass/ClassDCT.cs
@@ -1,17 +1,18 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using ImageMagick;
 
 namespace DCTClass;
 
 public class ClassDCT
 {
-    private static int UParam = 4;  //Wyjaśnienie w funkcji HideBit, dlaczego wybieramy te bloki
-    private static int VParam = 3;  //do ukrycia bitów
+    private static int UParam = 4;
+    private static int VParam = 3;
     private static string Terminator = "<END>";
-    private static double QScale = 30.0; //współczynnik skalujący do kwantyzacji
-                                         //wyjaśnienie również w funkcji HideBit
+    private static double QScale = 30.0;
 
-    //Funkcja zaminiajaca wiadomość do ukrycia na tablicę bitów
     private static List<int> StringToBits(string message)
     {
         List<int> bits = new List<int>();
@@ -26,7 +27,6 @@ public class ClassDCT
         return bits;
     }
 
-    //Funkcja sprawdzająca czy w tablicy bajtów znajduje się terminator
     private static bool CheckForTerminator(List<byte> bytes)
     {
         byte[] termBytes = Encoding.UTF8.GetBytes(Terminator);
@@ -39,47 +39,53 @@ public class ClassDCT
         }
         return true;
     }
-    
-    //Funkcja pobierająca bloki 8x8 pikseli z obrazu i konwertująca je do YCbCr
-    private static void GetBlocks(Bitmap bmp, int startX, int startY, double[,] blkY, double[,] blkCr, double[,] blkCb)
+
+    private static void GetBlocks(IPixelCollection<byte> pixels, int width, int height, int startX, int startY, double[,] blkY, double[,] blkCr, double[,] blkCb)
     {
         for (int y = 0; y < 8; y++)
         {
             for (int x = 0; x < 8; x++)
             {
-                if (startX + x >= bmp.Width || startY + y >= bmp.Height) continue;
-                Color c = bmp.GetPixel(startX + x, startY + y);
-                blkY[x, y] = 0.299 * c.R + 0.587 * c.G + 0.114 * c.B;
-                blkCb[x, y] = 128 - 0.168736 * c.R - 0.331264 * c.G + 0.5 * c.B;
-                blkCr[x, y] = 128 + 0.5 * c.R - 0.418688 * c.G - 0.081312 * c.B;
+                if (startX + x >= width || startY + y >= height) continue;
+
+                var pixelColor = pixels.GetPixel(startX + x, startY + y).ToColor();
+
+                double r = ((double)pixelColor.R / Quantum.Max) * 255.0;
+                double g = ((double)pixelColor.G / Quantum.Max) * 255.0;
+                double b = ((double)pixelColor.B / Quantum.Max) * 255.0;
+
+                blkY[x, y] = 0.299 * r + 0.587 * g + 0.114 * b;
+                blkCb[x, y] = 128 - 0.168736 * r - 0.331264 * g + 0.5 * b;
+                blkCr[x, y] = 128 + 0.5 * r - 0.418688 * g - 0.081312 * b;
             }
         }
     }
 
-    //Funkcja ustawiająca bloki 8x8 pikseli w obrazie na podstawie wartości YCbCr ~ odwrotna do GetBlocks
-    private static void SetBlocks(Bitmap bmp, int startX, int startY, double[,] blkY, double[,] blkCr, double[,] blkCb)
+    private static void SetBlocks(IPixelCollection<byte> pixels, int width, int height, int startX, int startY, double[,] blkY, double[,] blkCr, double[,] blkCb)
     {
         for (int x = 0; x < 8; x++)
         {
             for (int y = 0; y < 8; y++)
             {
-                if (startX + x >= bmp.Width || startY + y >= bmp.Height) continue;
+                if (startX + x >= width || startY + y >= height) continue;
+
                 double yValue = blkY[x, y];
                 double cbValue = blkCb[x, y];
                 double crValue = blkCr[x, y];
+
                 int r = (int)Math.Round(yValue + 1.402 * (crValue - 128));
                 int g = (int)Math.Round(yValue - 0.344136 * (cbValue - 128) - 0.714136 * (crValue - 128));
                 int b = (int)Math.Round(yValue + 1.772 * (cbValue - 128));
+
                 r = Math.Max(0, Math.Min(255, r));
                 g = Math.Max(0, Math.Min(255, g));
                 b = Math.Max(0, Math.Min(255, b));
-                Color c = Color.FromArgb(r, g, b);
-                bmp.SetPixel(startX + x, startY + y, c);
+
+                pixels.SetPixel(startX + x, startY + y, new byte[] { (byte)r, (byte)g, (byte)b });
             }
         }
     }
-    
-    //Funkcja wykonująca dyskretną transformatę kosinusową (DCT) na bloku 8x8
+
     private static double[,] dct8x8(double[,] block)
     {
         double[,] output = new double[8, 8];
@@ -105,8 +111,7 @@ public class ClassDCT
         }
         return output;
     }
-    
-    //Funkcja wykonująca odwrotną dyskretną transformatę kosinusową (IDCT) na bloku 8x8
+
     private static double[,] idct8x8(double[,] block)
     {
         double[,] output = new double[8, 8];
@@ -129,121 +134,120 @@ public class ClassDCT
         }
         return output;
     }
-    
-    //Funkcja ukrywająca pojedynczy bit w wybranym współczynniku DCT
-    //Tutaj używamy współczynnika (4,3) do ukrywania bitów
-    //Stosowanie wartości współczynnika o średniej częstotliwości pomaga zminimalizować widoczność zmian w obrazie
-    //Wykonujemy kwantyzację wartości współczynnika, a następnie modyfikujemy jego parzystość, aby ukryć bit
-    //Dzięki temu zmiany w obrazie przetrwają kompresję JPEG
-    private static void HideBit(double [,] dctBlock, int bit)
+
+    private static void HideBit(double[,] dctBlock, int bit)
     {
         double val = dctBlock[UParam, VParam];
         int quantized = (int)Math.Round(val / QScale);
         if (Math.Abs(quantized) % 2 != bit)
         {
-            if (quantized >= 0)
-            {
-                quantized++;
-            }
-            else 
-            {
-                quantized--;
-            }
+            if (quantized >= 0) quantized++;
+            else quantized--;
         }
-
         dctBlock[UParam, VParam] = quantized * QScale;
     }
-    
-    //Funkcja odczytująca pojedynczy ukryty bit z wybranego współczynnika DCT
-    private static int GetHiddenBit(double [,] dctBlock)
+
+    private static int GetHiddenBit(double[,] dctBlock)
     {
         double val = dctBlock[UParam, VParam];
         int quantized = (int)Math.Round(val / QScale);
         return Math.Abs(quantized) % 2;
     }
-    
-    //Główna funkcja szyfrująca wiadomość w obrazie
-    public static Image Encrypt(Image input, string message)
+
+    public static byte[] Encrypt(Stream inputStream, string message)
     {
-        Bitmap bmpIn = new Bitmap(input);
-        Bitmap bmpOut = new Bitmap(bmpIn); //kopiujemy obraz wejściowy
-        message += Terminator;  //dodajemy terminator na końcu wiadomości
+        using var image = new MagickImage(inputStream);
+
+        image.Format = MagickFormat.Png;
+
+        message += Terminator;
         List<int> bits = StringToBits(message);
         int bitIndex = 0;
         int bitMessageLength = bits.Count;
-        int width = bmpIn.Width - (bmpIn.Width % 8);
-        int height = bmpIn.Height - (bmpIn.Height % 8);
-        double[,] blockY = new double[8, 8];        //Tablice na bloki Y, Cb, Cr
+
+        int width = (int)image.Width - ((int)image.Width % 8);
+        int height = (int)image.Height - ((int)image.Height % 8);
+
+        double[,] blockY = new double[8, 8];
         double[,] blockCb = new double[8, 8];
         double[,] blockCr = new double[8, 8];
 
         bool finished = false;
 
-        for (int by = 0; by < height; by += 8)
+        using (var pixels = image.GetPixels())
         {
-            for (int bx = 0; bx < width; bx += 8)
+            for (int by = 0; by < height; by += 8)
             {
-                if (finished) break;
+                for (int bx = 0; bx < width; bx += 8)
+                {
+                    if (finished) break;
 
-                GetBlocks(bmpIn, bx, by, blockY,  blockCr,  blockCb);
-                double [,] dctY = dct8x8(blockY);
-                
-                if (bitIndex < bitMessageLength)
-                {
-                    int bit = bits[bitIndex];
-                    HideBit(dctY, bit);
-                    bitIndex++;
-                    double [,] idctY = idct8x8(dctY);
-                    SetBlocks(bmpOut, bx, by, idctY, blockCr, blockCb);
+                    GetBlocks(pixels, (int)image.Width, (int)image.Height, bx, by, blockY, blockCr, blockCb);
+                    double[,] dctY = dct8x8(blockY);
+
+                    if (bitIndex < bitMessageLength)
+                    {
+                        int bit = bits[bitIndex];
+                        HideBit(dctY, bit);
+                        bitIndex++;
+                        double[,] idctY = idct8x8(dctY);
+                        SetBlocks(pixels, (int)image.Width, (int)image.Height, bx, by, idctY, blockCr, blockCb);
+                    }
+                    else
+                    {
+                        finished = true;
+                    }
                 }
-                else
-                {
-                    finished = true;
-                }
+                if (finished) break;
             }
-            if (finished) break;
         }
 
-        return (Image)bmpOut;
+        return image.ToByteArray();
     }
 
-    //Główna funkcja odszyfrowująca wiadomość z obrazu
-    public static string Decrypt(Image input)
+    public static string Decrypt(Stream inputStream)
     {
-        Bitmap bmpIn = new Bitmap(input);
-        int width = bmpIn.Width - (bmpIn.Width % 8);
-        int height = bmpIn.Height - (bmpIn.Height % 8);
+        using var image = new MagickImage(inputStream);
+
+        int width = (int)image.Width - ((int)image.Width % 8);
+        int height = (int)image.Height - ((int)image.Height % 8);
+
         double[,] blockY = new double[8, 8];
         double[,] blockCb = new double[8, 8];
         double[,] blockCr = new double[8, 8];
-        
+
         byte currentByte = 0;
         int bitCount = 0;
         List<byte> collectedBytes = new List<byte>();
         bool foundTerminator = false;
 
-        for (int by = 0; by < height; by += 8)
+        using (var pixels = image.GetPixels())
         {
-            for (int bx = 0; bx < width; bx += 8)
+            for (int by = 0; by < height; by += 8)
             {
-                GetBlocks(bmpIn, bx, by, blockY,  blockCr,  blockCb);
-                double [,] dctY = dct8x8(blockY);
-                int bit = GetHiddenBit(dctY);
-                currentByte = (byte)((currentByte << 1) | bit);
-                bitCount++;
-                if (bitCount == 8)
+                for (int bx = 0; bx < width; bx += 8)
                 {
-                    collectedBytes.Add(currentByte);
-                    bitCount = 0;
-                    currentByte = 0;
-                    if (CheckForTerminator(collectedBytes))
+                    GetBlocks(pixels, (int)image.Width, (int)image.Height, bx, by, blockY, blockCr, blockCb);
+                    double[,] dctY = dct8x8(blockY);
+                    int bit = GetHiddenBit(dctY);
+
+                    currentByte = (byte)((currentByte << 1) | bit);
+                    bitCount++;
+
+                    if (bitCount == 8)
                     {
-                        foundTerminator = true;
-                        break;
+                        collectedBytes.Add(currentByte);
+                        bitCount = 0;
+                        currentByte = 0;
+                        if (CheckForTerminator(collectedBytes))
+                        {
+                            foundTerminator = true;
+                            break;
+                        }
                     }
                 }
+                if (foundTerminator) break;
             }
-            if (foundTerminator) break;
         }
 
         if (foundTerminator)
@@ -251,7 +255,7 @@ public class ClassDCT
             int termLen = Encoding.UTF8.GetByteCount(Terminator);
             collectedBytes.RemoveRange(collectedBytes.Count - termLen, termLen);
         }
-        
+
         return Encoding.UTF8.GetString(collectedBytes.ToArray());
     }
 }
